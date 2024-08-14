8431ede4370bd1288c875324cefbcc1614ed93ef
Chen-Pang He <chen.pang.he@jdh8.org>
2024-08-15 02:30:26 +0800

    Fix most of the things

--- build.rs
+++ build.rs
@@ -1,5 +1,18 @@
 use anyhow::Context as _;
 
+fn get_source_files<'a>(
+    dirs: glob::Paths,
+    suffix: impl AsRef<std::ffi::OsStr> + 'a,
+) -> impl Iterator<Item = std::path::PathBuf> + 'a {
+    dirs.flatten().filter_map(move |mut path| {
+        let stem = path.file_stem()?;
+        let mut name = stem.to_owned();
+        name.push(suffix.as_ref());
+        path.push(name);
+        path.is_file().then_some(path)
+    })
+}
+
 fn main() -> anyhow::Result<()> {
     let out_dir = std::env::var_os("OUT_DIR").context("OUT_DIR not set")?;
 
@@ -10,9 +23,12 @@ fn main() -> anyhow::Result<()> {
         .generate()?
         .write_to_file(std::path::PathBuf::from(out_dir).join("bindings.rs"))?;
 
+    let f32_sources = get_source_files(glob::glob("vendor/src/binary32/*/")?, "f.c");
+    let f64_sources = get_source_files(glob::glob("vendor/src/binary64/*/")?, ".c");
+
     cc::Build::new()
-        .files(glob::glob("vendor/src/binary{32,64}/*/*.c")?.flatten())
-        .std("c11")
+        .files(f32_sources.chain(f64_sources))
+        .include("include")
         .try_compile("m")?;
 
     Ok(())
